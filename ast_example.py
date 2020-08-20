92114aaa5b4389653b14b4c9bbd98369ec3d704b
liheng <liheng@6estates.com>
2020-08-20 18:26:52 +0800

    feat(project): add remove_field_declaration and add_field_declaration function

--- ast_example.py
+++ ast_example.py
@@ -1,7 +1,5 @@
-from pprint import pprint
-import ast
-F_MT700_30 = 2
-F_MT700_31 = 3
+# F_MT700_30 = 2
+# F_MT700_31 = 3
 a = max(F_MT700_30, F_MT700_31)
 if (a%2==1):
   print('odd')
--- main.py
+++ main.py
@@ -1,44 +1,67 @@
-from pprint import pprint
-import inspect
+import re
 import ast
 import astunparse
 
-def main():
-    with open("ast_example.py", "r") as source:
-        tree = ast.parse(source.read())
-    # astunparse.dump(tree)
-    print(astunparse.dump(tree))
 
+def remove_field_declaration(code):
+    """
+    case 1:
+    input
+    F_MT700_7 = Field('F_MT700_7')
+    return inEmpty(F_MT700_7)
+
+    output
+    return inEmpty(F_MT700_7)
+
+    @param {string} code
+    @return {string}
+    """
+    tree = ast.parse(code)
     analyzer = Analyzer()
     analyzer.visit(tree)
-    analyzer.report()
+    def filter_node(node):
+        return not(isinstance(node, ast.Assign) and node.targets[0].id in analyzer.name)
+    tree.body = filter(filter_node, tree.body)
+    return astunparse.unparse(tree)
+
+
+def add_field_declaration(code):
+    """
+    case 1:
+    input
+    return inEmpty(F_MT700_7)
+
+    output
+    F_MT700_7 = Field('F_MT700_7')
+    return inEmpty(F_MT700_7)
+
+    @param {string} code
+    @return {string}
+    """
+    tree = ast.parse(code)
+    analyzer = Analyzer()
+    analyzer.visit(tree)
+    for field in analyzer.name:
+        tree.body.insert(0, ast.parse('%s = Field("%s")' % (field, field)).body[0])
+    return astunparse.unparse(tree)
+
+
+def main():
+    with open("ast_example.py", "r") as source:
+        code = source.read()
+    code = add_field_declaration(code)
+    print(code)
+    code = remove_field_declaration(code)
+    print(code)
 
 
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
-        self.stats = {
-            "import": [],
-            "from": [],
-            "variable": []
-        }
+        self.name = []
 
-    def visit_Import(self, node):
-        for alias in node.names:
-            self.stats["import"].append(alias.name)
-        self.generic_visit(node)
-
-    def visit_ImportFrom(self, node):
-        for alias in node.names:
-            self.stats["from"].append(alias.name)
-        self.generic_visit(node)
-
-    def visit_Variable(self, node):
-        for alias in node.names:
-            self.stats["variable"].append(alias.name)
-        self.generic_visit(node)
-
-    def report(self):
-        pprint(self.stats)
+    def visit_Name(self, node):
+        if re.match(r"((AINS)|(BINS)|F)_\w+_\w+", node.id):
+            self.name.append(node.id)
 
 
 if __name__ == "__main__":
